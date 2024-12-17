8ea29c2103d910165d7af582edf933d0ef84e5ed
waheedtopi70 <waheedtopi748484@hotmail.com>
2024-12-17 15:05:12 +0500

    Add files via upload

--- safeum.py
+++ safeum.py
@@ -1,6 +1,6 @@
 import os, sys, platform
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import safeum
+    import safeum_enc
 elif bit == '32bit':
-    import safeum
+    import safeum_enc
